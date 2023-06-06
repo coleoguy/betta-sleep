d280a9de91ee61cb5330e7b9528b36dd0bf648d0
Andresdbp <71858994+Andresdbp@users.noreply.github.com>
2023-06-06 13:57:46 -0500

    adding plot script

---
+++ scripts/plot.R
@@ -0,0 +1,17 @@
+# Andres Barboza
+# 06/06/2023
+
+# Reading in data
+
+# Theme
+ggtheme <- theme_bw() + theme(panel.grid.major = element_blank(),
+                              panel.grid.minor = element_blank(),
+                              panel.background = element_blank(),
+                              panel.border=element_blank(),
+                              axis.line = element_line(colour="grey30"),
+                              axis.title = element_text(colour="grey20"),
+                              axis.text = (element_text(colour="grey30")),
+                              legend.title = element_text(colour="grey20"),
+                              legend.text = element_text(colour="grey30"))
+
+# 
