28dc2d21a977192d7c86b3a7aefae53be3b217d0
Andresdbp <71858994+Andresdbp@users.noreply.github.com>
2023-06-14 18:10:01 -0500

    Update playing with data.R

--- scripts/playing with data.R
+++ scripts/playing with data.R
@@ -1,15 +1,19 @@
 
 library(ggplot2)
-dat <- rbind(read.csv("yp.csv"), read.csv("sr.csv"), read.csv("wtbs.csv"))[,-1]
-dat$strains <- as.factor(c(rep("yellow plakat", 8), rep("super red", 6), rep("wild-type", 8)))
+library(dplyr)
+dat <- rbind(read.csv("results/yp.csv"), read.csv("results/sr.csv"), read.csv("results/wtbs.csv"))
+dat$strains <- as.factor(c(rep("Yellow Plakat", 8), rep("Super Red", 6), rep("Wild-type", 8)))
 dat <- data.frame(c(dat$day,dat$twilight,dat$night),
-                  c(factor(rep(c("day","twilight","night"), each=22))),
+                  c(factor(rep(c("Day","Twilight","Night"), each=22))),
                   c(rep(dat$strains, times=3)))
-colnames(dat) <- c("activity", "time", "strain")
-dat$time <- factor(dat$time, levels = c("day","twilight","night"))
-ggplot(dat, aes(x=strain,y=activity, color=time)) +
-  geom_dotplot(binaxis = "y",stackdir = "center",position="dodge") +
+colnames(dat) <- c("Locomotion", "Time", "Strain")
+dat$Time <- factor(dat$Time, levels = c("Day","Twilight","Night"))
+
+
+ggplot(dat, aes(x=Strain,y=Locomotion, fill=Time)) +
+  geom_dotplot(binaxis = "y",stackdir = "center",position="dodge",dotsize=0.7) +
   theme_bw() +
-  scale_fill_manual(breaks = c("day", "twilight", "night"), 
-                    values=c("red", "blue", "green"))
+  scale_fill_manual(values = c("#B9DBF4", "#aaAAD3","#155289"))+
+  stat_summary(fun = mean, geom = "point", shape = 95, size = 10, color = "#ff0000", position = position_dodge(width = 0.9))+
+  guides(fill = guide_legend(override.aes = list(shape = NA)))
 
